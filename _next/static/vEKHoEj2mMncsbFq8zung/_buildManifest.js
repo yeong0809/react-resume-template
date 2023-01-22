108e5ae38848da72092f0ffb93512504d0959fb3
yeong0809 <31876283+yeong0809@users.noreply.github.com>
2023-01-22 05:17:14 +0000

    Deploying to gh-pages from @ yeong0809/react-resume-template@b0bc25d00266ba2fe2ffa33a07cd41419cba40a0 🚀

---
+++ _next/static/vEKHoEj2mMncsbFq8zung/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/249-f0aacd6fbc7965e4.js","static/chunks/pages/index-c727d235ef021778.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
