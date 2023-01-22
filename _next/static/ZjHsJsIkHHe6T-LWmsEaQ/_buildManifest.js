db898f8705f6d7f5ca97ea3377ee66927ab898db
yeong0809 <31876283+yeong0809@users.noreply.github.com>
2023-01-22 05:49:17 +0000

    Deploying to gh-pages from @ yeong0809/react-resume-template@2982c1ed9b0b2eea57bb8ab0ea768a851204c115 🚀

---
+++ _next/static/ZjHsJsIkHHe6T-LWmsEaQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/249-a8e1bcbf51a60725.js","static/chunks/pages/index-e25d2b11714e256c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/l6pm3vevxZXx7sFl6AuF0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/249-060307fc42585276.js","static/chunks/pages/index-e25d2b11714e256c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
